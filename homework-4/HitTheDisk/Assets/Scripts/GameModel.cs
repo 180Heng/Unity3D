0403e0fb839939274b4935b02bf0d04b20d936e1
Heng <964683913@qq.com>
2018-04-17 23:59:34 +0800

    update

---
+++ homework-4/HitTheDisk/Assets/Scripts/GameModel.cs
@@ -0,0 +1,48 @@
+﻿
+public class GameModel : IScore {
+
+    public int Score { get; private set; }
+    public int Round { get; private set; }
+    private static GameModel gameModel;
+
+    private GameModel() {
+        Round = 0;
+    }
+
+    public static GameModel GetGameModel() {
+        return gameModel ?? (gameModel = new GameModel());
+    }
+
+    public void AddScore() {
+		if(Round%3==1)
+        	Score += 10;
+		else Score += 20;
+        if (CheckUpdate()) {
+            Round++;
+            FirstSceneControllerBase.GetFirstSceneControllerBase().Update();
+        }
+    }
+
+	public void AddScoreBonus() {
+		
+		if (CheckUpdate()) {
+			Round++;
+			FirstSceneControllerBase.GetFirstSceneControllerBase().Update();
+		}
+	}
+
+    public void SubScore() {
+        Score -= 10;
+        if (Score < 0) {
+            FirstSceneControllerBase.GetFirstSceneControllerBase().SetGameStatus(GameStatus.Lose);
+        }
+    }
+
+    public bool CheckUpdate() {
+        return Score >= Round * 20;
+    }
+
+    public int GetScore() {
+        return GameModel.GetGameModel().Score;
+    }
+}
---
+++ homework-4/HitTheDisk/Assets/Scripts/UI.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UI : MonoBehaviour {
+    GameObject scoreText;
+    GameObject gameStatuText;
+    IScore score = FirstSceneControllerBase.GetFirstSceneControllerBase() as IScore;
+    IQueryStatus gameStatu = FirstSceneControllerBase.GetFirstSceneControllerBase() as IQueryStatus;
+
+    // Use this for initialization
+    void Start() {
+        scoreText = GameObject.Find("Score");
+        gameStatuText = GameObject.Find("GameStatu");
+    }
+
+    // Update is called once per frame
+    void Update() {
+        string score = Convert.ToString(this.score.GetScore());
+        if (gameStatu.QueryGameStatus() == GameStatus.Lose) {
+            gameStatuText.GetComponent<Text>().text = "Game Over!";
+        } else if (gameStatu.QueryGameStatus() == GameStatus.Win) {
+            gameStatuText.GetComponent<Text>().text = "You Win!";
+        }
+        scoreText.GetComponent<Text>().text = "按空格键发射飞碟.\n" +
+            "Score: " + score;
+    }
+}
