8926c7a2354675f63176736d0e42f59a96f04e78
Heng <964683913@qq.com>
2018-06-07 00:26:13 +0800

    update

---
+++ homework-8/Assets/act.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class act : MonoBehaviour {
+
+   public int frame;
+   public float height;
+   private Button btn;
+   public Text text;
+  
+  
+  IEnumerator TextCollapsed() {
+    float y = height;
+    for (int i = 0; i < frame; ++i) {
+      y -= height / frame;
+      text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0, text.rectTransform.sizeDelta.x);
+      text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, y);
+      if (i == frame - 1) {
+        text.gameObject.SetActive(false);
+      }
+      yield return null;
+    }
+  }
+
+  IEnumerator TextVisible() {
+    float y = 0;
+    for (int i = 0; i < frame; ++i) {
+      y += height / frame;
+      text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Right, 0, text.rectTransform.sizeDelta.x);
+      text.rectTransform.SetInsetAndSizeFromParentEdge(RectTransform.Edge.Bottom, 0, y);
+      if (i == 0) {
+        text.gameObject.SetActive(true);
+      }
+      yield return null;
+    }
+  }
+
+  void OnClick() {
+    if (text.gameObject.activeSelf) {
+      StartCoroutine(TextCollapsed());
+    } else {
+      StartCoroutine(TextVisible());
+    }
+  }
+    // Use this for initialization  
+    void Start()
+    {
+        btn = this.gameObject.GetComponent<Button>();
+        btn.onClick.AddListener(OnClick);
+        StartCoroutine(TextCollapsed());
+    }
+
+}
--- homework-8/README.md
+++ homework-8/README.md
@@ -1,2 +1,9 @@
 # Unity-3D-homework8
 -----
+视频：https://pan.baidu.com/s/1_WCrB2jj4V9rXBwbNej_7g
+
+--------
+公告牌作业效果图：
+![](http://a4.qpic.cn/psb?/V13ncXZC2IVZm7/RcfIzLlgWhVVhHQ1DD5AHfcpgvbC93thQXMdI73Qfq8!/b/dDMBAAAAAAAA&ek=1&kp=1&pt=0&bo=egT8AQAAAAARF6M!&tl=3&vuin=964683913&tm=1528300800&sce=60-2-2&rf=viewer_4)
+
+-----
